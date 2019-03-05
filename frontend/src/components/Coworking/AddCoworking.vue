8318ef6088541b19cd33a5f69eb82c76a00c31b4
Leon <heiko.avalos@gmail.com>
2019-03-05 15:14:57 -0500

    Creado componente para creacion coworking

--- frontend/src/components/Coworking/AddCoworking.vue
+++ frontend/src/components/Coworking/AddCoworking.vue
@@ -1,20 +1,58 @@
 <template>
-    <div>
+    <div id="app">
         <h1>Bienvenido : {{user}}</h1>
-        
+
+        <v-app id="inspire">
+            <legend>Agregar Coworking</legend>
+            <form>
+                <v-text-field
+                v-model="name"
+                label="Nombre"
+                ></v-text-field>
+
+                <v-text-field
+                v-model="address"
+                label="Direccion"
+                ></v-text-field>
+
+                <v-text-field
+                v-model="price"
+                label="Precio"
+                ></v-text-field>
+
+                <v-select
+                v-model="calification"
+                :items="items"
+                label="Calificacion"
+                ></v-select>
+
+                <v-btn color="info">Guardar</v-btn>
+
+                <v-btn disabled= true>Cancelar</v-btn>
+
+            </form>
+        </v-app>
     </div>
 </template>
 
 <script>
-export default {
+
+export default { 
     data() {
         return {
-            user: 'ROOT'
+            user: 'ROOT',
+            name: '',
+            address: '',
+            price: '',
+            calification: '',
+            items : [
+                1,2,3,4,5
+            ]
         }
-    },
-    
+    },    
 }
 </script>
+
 
 <style>
 
