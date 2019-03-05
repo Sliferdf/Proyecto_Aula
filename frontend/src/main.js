e8c21b93d3127a56ea6e7683995057d40c1f9464
Leon <heiko.avalos@gmail.com>
2019-03-05 15:18:37 -0500

    Añadida dependecia Vuelidate para validar formularios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,11 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import Vuelidate from 'vuelidate'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
+Vue.use(Vuelidate)
 
 /* eslint-disable no-new */
 new Vue({
